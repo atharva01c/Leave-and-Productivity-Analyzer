cf1c54ee5c46e19df89229ed4d1fc213ac711463
atharva01c <118446970+atharva01c@users.noreply.github.com>
2025-12-19 21:15:45 +0530

    update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-  global.prisma ||
+const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "warn"],
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
